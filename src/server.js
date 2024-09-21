aacaf59af0751174b627dbfd8bb1c166493bd8b7
Марія Драгнєва <dragnevamasa95@gmail.com>
2024-09-21 19:05:36 +0300

    Add html, css, js

--- src/server.js
+++ src/server.js
@@ -2,14 +2,22 @@
 
 const moment = require("moment")
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const PORT = 8000
 const HOST = 'localhost'
 
+app.use('/static/', express.static(path.join(__dirname, 'static'))) 
+
 function getCurrentDay(){
     return moment().format('MMMM Do YYYY, h:mm:ss a');
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./templates/index.html"))
+})
+
 
 app.get('/date', (req, res) => {
     console.log("someone visited the page")
@@ -17,6 +25,7 @@ app.get('/date', (req, res) => {
     console.log(currentDate)
     res.send(`${currentDate}`)
 })
+
 app.listen(PORT, HOST, () =>{
     console.log(`Server is running at http://${HOST}:${PORT}`);
 })
